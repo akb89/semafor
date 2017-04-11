0e2cef3bfae2c0b359966b559df57fcc449ae31e
Alex Kabbach <akabbach@gmail.com>
2017-04-11 11:00:51 +0200

    Refactored prepare script for MST parsing of testing splits

--- bin/prepare/prepare.sh
+++ bin/prepare/prepare.sh
@@ -111,10 +111,13 @@ pushd ${MST_PARSER_HOME}
 time ${JAVA_HOME_BIN}/java \
     -classpath ".:./lib/trove.jar:./lib/mallet-deps.jar:./lib/mallet.jar" \
 	-Xms${min_ram} \
-	-Xmx${max_ram} mst.DependencyParser \
-	test separate-lab \
+	-Xmx${max_ram} \
+	mst.DependencyParser \
+	test \
+	separate-lab \
 	model-name:${mst_parser_model} \
-	decode-type:proj order:2 \
+	decode-type:proj \
+	order:2 \
 	test-file:${mst_conll_input_testing_sentence_splits} \
 	output-file:${mstparsed_testing_sentence_splits} \
 	format:CONLL
