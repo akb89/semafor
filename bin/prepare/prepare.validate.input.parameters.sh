7aa706d30fd635a3602c22341414e59abe2c08fb
Alex Kabbach <akabbach@gmail.com>
2017-04-20 18:27:32 +0200

    echoing input parameters during preprocessing

--- bin/prepare/prepare.validate.input.parameters.sh
+++ bin/prepare/prepare.validate.input.parameters.sh
@@ -16,6 +16,8 @@ case "${dependency_parser}" in
         echo "Invalid dependency_parser parameter: ${dependency_parser}. Should be MST, MALT or TURBO"
         exit 1
 esac
+echo "  dependency_parser = ${dependency_parser}"
+
 echo "Validating with_exemplars parameter..."
 case "${with_exemplars}" in
     true )
@@ -26,6 +28,8 @@ case "${with_exemplars}" in
         echo "Invalid with_exemplars parameter: ${with_exemplars}. Should be true or false"
         exit 1
 esac
+echo "  with_exemplars = ${with_exemplars}"
+
 echo "Validating clean_after_preprocessing parameter..."
 case "${clean_after_preprocessing}" in
     true )
@@ -36,6 +40,7 @@ case "${clean_after_preprocessing}" in
         echo "Invalid clean_after_preprocessing parameter: ${clean_after_preprocessing}. Should be true or false"
         exit 1
 esac
+echo "  clean_after_preprocessing = ${clean_after_preprocessing}"
 
 echo
 echo "All input parameters are valid"
