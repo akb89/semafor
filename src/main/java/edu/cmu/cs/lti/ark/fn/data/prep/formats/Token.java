a288e5fd36d8187e610ea6ac55e8b61fda5f46e9
akb89 <akb@3azouz.net>
2017-07-10 23:39:39 +0200

    Debugging

--- src/main/java/edu/cmu/cs/lti/ark/fn/data/prep/formats/Token.java
+++ src/main/java/edu/cmu/cs/lti/ark/fn/data/prep/formats/Token.java
@@ -246,18 +246,17 @@ public class Token {
 	public static Token fromConll(String line) {
 		final String[] fields = line.trim().split("\t");
 		checkArgument(fields.length == 10, "ConllToken must have 10 \"\\t\"-separated fields");
+		System.err.println("id = " + parseConllField(fields[0], parseInt));
+		System.err.println("form = " + fields[1]);
+		System.err.println("lemma = " + parseConllField(fields[2], parseStr));
+		System.err.println("cpostag = " + parseConllField(fields[3], parseStr));
+		System.err.println("feats = " + parseConllField(fields[5], parseStr));
+		System.err.println("head = " + parseConllField(fields[6], parseInt));
+		System.err.println("deprel = " + parseConllField(fields[7], parseStr));
+		System.err.println("phead_str = " + parseConllField(fields[8], parseStr));
+		System.err.println("phead = " + parseConllField(fields[8], parseInt));
+		System.err.println("pdeprel = " + parseConllField(fields[9], parseStr));
 		return new Token(
-				System.err.println("id = " + parseConllField(fields[0], parseInt));
-				System.err.println("form = " + fields[1]);
-				System.err.println("lemma = " + parseConllField(fields[2], parseStr));
-				System.err.println("cpostag = " + parseConllField(fields[3], parseStr));
-				System.err.println("feats = " + parseConllField(fields[5], parseStr));
-				System.err.println("head = " + parseConllField(fields[6], parseInt));
-				System.err.println("deprel = " + parseConllField(fields[7], parseStr));
-				System.err.println("phead_str = " + parseConllField(fields[8], parseStr));
-				System.err.println("phead = " + parseConllField(fields[8], parseInt));
-				System.err.println("pdeprel = " + parseConllField(fields[9], parseStr));
-
 				parseConllField(fields[0], parseInt),  // id
 				fields[1],  // form
 				parseConllField(fields[2], parseStr),  // lemma
